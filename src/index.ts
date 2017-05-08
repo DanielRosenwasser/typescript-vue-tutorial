2d517d921e94276708b5a845567b9023a0943aed
Daniel Rosenwasser <Daniel.Rosenwasser@microsoft.com>
2017-05-07 20:19:21 -0700

    Initial commit.

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import Vue from "vue";
+
+let v = new Vue({
+    el: "#app",
+    template: `
+    <div>
+        <div>Hello {{name}}!</div>
+        Name: <input v-model="name" type="text">
+    </div>`,
+    data: {
+        name: "World"
+    }
+});
