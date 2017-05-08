841fa68abaf1d4d601655e0120d0244d05b0b0bd
Daniel Rosenwasser <Daniel.Rosenwasser@microsoft.com>
2017-05-08 11:15:01 -0700

    Added a single-file Vue component.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import Vue from "vue";
+import HelloComponent from "./components/Hello.vue";
 
 let v = new Vue({
     el: "#app",
     template: `
     <div>
-        <div>Hello {{name}}!</div>
         Name: <input v-model="name" type="text">
-    </div>`,
-    data: {
-        name: "World"
+        <hello-component :name="name" :initialEnthusiasm="5" />
+    </div>
+    `,
+    data: { name: "World" },
+    components: {
+        HelloComponent
     }
 });
